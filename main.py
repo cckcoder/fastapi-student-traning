ddd7accf30ee2d7c6331c06cc5d71604c60a1af9
codewiz <c.chaiwat.ch@gmail.com>
2021-09-05 15:19:14 +0700

    add: parameter to route.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,11 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return { 'message': 'Hello World'}
+
+
+@app.get("/ice_cream/{id}")
+def read_ice_cream(
+    id: int, q: Optional[str] = None
+):
+    return { 'ice_cream_id': id, 'q': q } 
+    
