1267de0367b7bb1d35bdc61daf805de68a232ba8
codewiz <c.chaiwat.ch@gmail.com>
2021-09-11 20:39:15 +0700

    add: cors

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routers import icecreams
 from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
+
+origins = [
+    "*",
+    "http://localhost",
+    "http://127.0.0.1",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(icecreams.router)
 
